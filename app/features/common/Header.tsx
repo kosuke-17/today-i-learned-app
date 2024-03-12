9e6e30e60cdf67af4bed19b7201da76d27437fc0
kosuke-17 <tamutamu17@icloud.com>
2024-03-12 21:45:42 +0900

    create Header

---
+++ app/features/common/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="h-20 flex justify-center items-center bg-emerald-600">
+      <p className=" text-2xl font-bold">毎日学習記録</p>
+    </header>
+  )
+}
+
+export default Header
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ArticleList from './features/articles/ArticleList'
 
 export default async function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ArticleList />
     </main>
   )
