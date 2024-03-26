00a8d0e80a5d4e8547d4161649e421c36eaecc62
kosuke-17 <tamutamu17@icloud.com>
2024-03-26 15:19:50 +0900

    fix css

--- app/_features/common/MainSideNav/index.tsx
+++ app/_features/common/MainSideNav/index.tsx
@@ -20,10 +20,10 @@ export default function MainSideNav() {
   return (
     <aside
       id="main-sidenav"
-      className="fixed top-0 left-0 w-20 h-screen"
+      className="fixed top-0 left-0 w-20 h-screen pt-4 bg-emerald-500"
       aria-label="MainSideNav"
     >
-      <div className="h-full py-4 overflow-y-auto bg-emerald-500">
+      <div className="h-full overflow-y-auto">
         <ul className="space-y-1">
           {navs.map((nav) => (
             <li key={nav.name}>
--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 
-import ArticleNavBar from '@/app/_features/article/ArticleNavBar'
 import MainSideNav from '@/app/_features/common/MainSideNav'
 
 export const metadata: Metadata = {
@@ -15,9 +14,8 @@ export default function Layout({
   return (
     <>
       <MainSideNav />
-      <div className="h-screen w-screen flex bg-gray-100">
-        <ArticleNavBar />
-        <div className="w-full flex-5">{children}</div>
+      <div className="ml-20 h-screen w-screen bg-gray-100">
+        <div className="h-full flex-5">{children}</div>
       </div>
     </>
   )
