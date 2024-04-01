62ba8744f4ec1e98f30fd77b175585c4f8d5fafd
kosuke-17 <tamutamu17@icloud.com>
2024-04-01 17:56:57 +0900

    fix

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -16,13 +16,11 @@ export default async function Page({ params }: Props) {
 
   return (
     <div className="flex h-full gap-2 mx-40">
-      <div className="flex-5 w-full h-full bg-white p-4 rounded-md">
-        <div className="bg-gray-200 text-center mb-3 text-3xl font-bold">
+      <div className="flex-5 w-full h-full bg-white py-4 px-8 rounded-md">
+        <div className="text-center mb-3 text-5xl font-bold">
           {article.title}
         </div>
-        <div className="bg-gray-100">
-          <Markdown content={article.content ? article.content : ''} />
-        </div>
+        <Markdown content={article.content ? article.content : ''} />
       </div>
       <div className="flex-1 bg-white rounded-md pt-4"></div>
     </div>
--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -8,13 +8,12 @@ type Props = {
 }
 
 /**
- * @see: https://github.com/remarkjs/react-markdown?tab=readme-ov-file
+ * @see: https://github.com/remarkjs/react-markdown?tab=readme-ov-file#react-markdown
  */
 export default function Markdown({ content }: Props) {
   return (
     <ReactMarkdown
-      className="markdown"
-      remarkPlugins={[remarkGfm]}
+      remarkPlugins={[remarkGfm]} // remarkGfmを適用させるには、contentの中身を適した形式にする必要があるため公式を確認する
       components={{
         a: Anchor,
         pre: Pre,
