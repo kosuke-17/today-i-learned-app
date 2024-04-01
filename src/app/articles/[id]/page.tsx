04cf52356404e0d98ba6aaa49ec5fce198a6d4ba
kosuke-17 <tamutamu17@icloud.com>
2024-04-01 15:24:06 +0900

    fix article detail

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -14,8 +14,17 @@ export default async function Page({ params }: Props) {
   }
 
   return (
-    <>
-      <div>{article.title}</div>
-    </>
+    <div className="flex h-full gap-2 mx-40">
+      <div className="flex-5 w-full h-full bg-white p-4 rounded-md">
+        <div className="bg-gray-200 text-center mb-3 text-3xl font-bold">
+          {article.title}
+        </div>
+        <div className="bg-gray-100">
+          <div>{article.content}</div>
+          <div>{article.content}</div>
+        </div>
+      </div>
+      <div className="flex-1 bg-white rounded-md pt-4"></div>
+    </div>
   )
 }
