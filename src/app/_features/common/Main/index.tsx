83cd5f2e6945fb6c87f2400fe713dde58a8ae3db
kosuke-17 <tamutamu17@icloud.com>
2024-04-01 12:52:53 +0900

    fix min screen size

--- src/app/_features/common/Main/index.tsx
+++ src/app/_features/common/Main/index.tsx
@@ -3,8 +3,9 @@ import { ReactNode } from 'react'
 type Props = { children: ReactNode }
 
 export default function Main({ children }: Props) {
+  // min-h-screenにすることで、最小が画面サイズでそれより大きい場合は大きいだけ下に追加されていく
   return (
-    <main className="h-screen w-screen flex bg-secondary-dark gap-2 p-2">
+    <main className="min-h-screen w-screen flex bg-secondary-dark gap-2 p-2">
       {children}
     </main>
   )
