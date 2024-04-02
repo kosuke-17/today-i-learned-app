1cfb094ffaaa12d028c9afc9ea862b0a9afa69ff
kosuke-17 <tamutamu17@icloud.com>
2024-04-02 18:56:15 +0900

    fix create nac

--- src/app/_features/article/SideNav/index.tsx
+++ src/app/_features/article/SideNav/index.tsx
@@ -1,4 +1,4 @@
-import { PlusIcon } from '@heroicons/react/24/outline'
+import { DocumentPlusIcon } from '@heroicons/react/24/outline'
 
 import _NavLink from '@/app/_features/article/_NavLink'
 import Divider from '@/app/_features/layout/Divider'
@@ -7,19 +7,18 @@ import { PATH } from '@/constant/path'
 import type { ArticleSelectForNav } from '@/lib/articles/definitions'
 import { fetchArticlesForNav } from '@/lib/articles/fetchs'
 
-const CreateNavIcon = (
-  <PlusIcon className="h-8 w-16 px-4 text-white hover:bg-primary-dark rounded-lg" />
+const CreateNav = (
+  <div className="flex items-center px-8 w-full gap-2 text-white hover:bg-primary-dark rounded-lg">
+    <DocumentPlusIcon className="h-5 w-5" />
+    作成
+  </div>
 )
 
 export default async function ArticleNavBar() {
   const articles: ArticleSelectForNav[] = await fetchArticlesForNav()
   return (
     <div className="animate-fade-in-left rounded-md w-44 bg-primary-main text-center pt-4">
-      <Link
-        textNode={CreateNavIcon}
-        href={PATH.ARTICLES_CREATE}
-        className="flex justify-center flex-1"
-      />
+      <Link textNode={CreateNav} href={PATH.ARTICLES_CREATE} className="" />
 
       <Divider />
 
